4ecce138c544c01eb0649e51c48c5d18b03c780b
Łukasza Abramczyk <abramspl@users.noreply.github.com>
2020-11-12 12:02:44 +0100

    Uruchomienie Aplikacji

---
+++ learning_log/learning_log/learning_logs/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Miejsce na Twoje modele.
+
+class Topic(models.Model):
+    """ Temat poznawany przez uzytkownika."""
+
+    text = models.CharField(max_length=200)
+    data_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Zwraca reprezentacje modelu w postaci ciagu tekstowgo."""
+
+        return self.text
+
+class Entry(models.Model):
+    """Konkretne informacje o postepie w nauce."""
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Zwraca reprezentacje modelu w postaci ciagu tekstowego."""
+
+        return f'{self.text[:50]}...'
