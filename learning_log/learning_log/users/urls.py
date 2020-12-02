ba172e17111100e90f42a157434eb9d600659e08
Łukasza Abramczyk <abramspl@users.noreply.github.com>
2020-12-02 13:48:52 +0100

    strona rejestracji uzytkownika

--- learning_log/learning_log/users/urls.py
+++ learning_log/learning_log/users/urls.py
@@ -2,8 +2,12 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 urlpatterns = [
     # Dolaczanie domyslnych adresow URL uwierzytelniania.
     path('', include('django.contrib.auth.urls')),
+    # Strona rejestracji.
+    url('register/', views.register, name='register'),
 ]
